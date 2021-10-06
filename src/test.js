269678c65db5421ecd7efcd02f5d439775a73af0
EladT <eladtal14@gmail.com>
2021-10-06 22:05:47 +0300

    mult foo by 8

--- src/test.js
+++ src/test.js
@@ -1,4 +1,7 @@
 export const foo = (number) => {
-    const multNum = number * 2;
+    const multNum = number * 2 * 8;
     return multNum;
+}
+export const x = () => {
+    console.log("12334", 12334);
 }
