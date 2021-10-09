92e36afda8bbee781fe3c3480487498922060f63
EladT <eladtal14@gmail.com>
2021-10-09 16:32:13 +0300

    fixed newFunc

--- src/test.js
+++ src/test.js
@@ -6,8 +6,9 @@ export const x = () => {
     console.log("12334", 12334);
 }
 export const newFunc = () => {
+    const testMe = 3;
     return new Date()
 }
 export const num = 2;
-export const number=3;
+export const number = 3;
 
